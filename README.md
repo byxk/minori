2615fdb894d2e52411ba968185a8a5f47f9424d1
Patrick Tseng <ptseng@appneta.com>
2017-10-10 15:31:23 -0700

    rmrss, rmshow, better logging

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # minori
 
 ### todo
-remove shows, rss
-scan rss
-add torrent to deluge with options
-add pre/post download hooks
-add pre/post scan hooks
+* remove shows, rss
+* scan rss
+* add torrent to deluge with options
+* add pre/post download hooks
+* add pre/post scan hooks
--- minori.py
+++ minori.py
@@ -3,13 +3,26 @@
 import argparse
 import sqlite3
 import datetime
+import sys
+import logging
 from pprint import pprint
+
+logging.basicConfig(filename='minori.log',
+                    filemode='a',
+                    format='%(asctime)s [%(levelname)s] %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 
 class Minori:
     def __init__(self, db='database.db'):
         self.db = db
         self.connection = sqlite3.connect(self.db)
+        self.logger = logging.getLogger('Minori')
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
     def __del__(self):
         self.connection.commit()
@@ -21,7 +34,7 @@ class Minori:
                      keywords text, date_added timestamp)''')
         self.connection.execute('''CREATE TABLE IF NOT EXISTS rss
              (name text primary key, url text, date_added timestamp)''')
-        print("initialize::Created database")
+        self.logger.info("Initialized database")
 
     def add_rss(self, name, url):
         date = datetime.datetime.now()
@@ -29,8 +42,20 @@ class Minori:
         try:
             self.connection.execute(sql_statement, (name, url, date))
         except sqlite3.IntegrityError:
-            print("add_show::RSS already exists in database")
+            self.logger.warning("RSS already exists in database")
             return
+
+        self.logger.info("Added RSS")
+
+    def rm_rss(self, name):
+        sql_statement = 'DELETE FROM rss WHERE name=?'
+        try:
+            for i in name:
+                self.connection.execute(sql_statement, (i,))
+        except sqlite3.IntegrityError:
+            self.logger.warning("RSS doesn't exist in database")
+            return
+        self.logger.info("Removed RSS")
 
     def add_show(self, name, max_ep, keywords, current=0):
         date = datetime.datetime.now()
@@ -38,10 +63,19 @@ class Minori:
         try:
             self.connection.execute(sql_statement, (name, int(max_ep), int(current), keywords, date))
         except sqlite3.IntegrityError:
-            print("add_show::Show already exists in database")
+            self.logger.warning("Show already exists in database")
             return
+        self.logger.info("Added show!")
 
-        print("add_show::Added show!")
+    def rm_show(self, name):
+        sql_statement = 'DELETE FROM shows WHERE name=?'
+        try:
+            for i in name:
+                self.connection.execute(sql_statement, (i,))
+        except sqlite3.IntegrityError:
+            self.logger.warning("Show doesn't exist in database")
+            return
+        self.logger.info("Removed show!")
 
     def get_all_shows(self):
         try:
@@ -50,7 +84,7 @@ class Minori:
             print("SHOWNAME | MAX_EPS | CURRENT_EP | DATE_ADDED")
             pprint(shows)
         except sqlite3.OperationalError:
-            print("Database not initialized")
+            self.logger.error("Database not initialized")
 
     def get_all_rss(self):
         try:
@@ -59,8 +93,7 @@ class Minori:
             print("RSS Name | RSS URL")
             pprint(rss)
         except sqlite3.OperationalError:
-            print("Database not initialized")
-
+            self.logger.error("Database not initialized")
 
 
 def main():
@@ -79,10 +112,18 @@ def main():
                                                   watched)', action='store_true')
     add_subparser.set_defaults(which='addshow')
 
+    rmshow_subparser = subparser.add_parser('rmshow', help='Remove a show')
+    rmshow_subparser.add_argument('name', help='Comma-separated list of shows to remove')
+    rmshow_subparser.set_defaults(which='rmshow')
+
     rss_subparser = subparser.add_parser('addrss', help='Add RSS subs')
     rss_subparser.add_argument('name', help='Name of the RSS feed')
     rss_subparser.add_argument('url', help='Direct to RSS feed')
     rss_subparser.set_defaults(which='addrss')
+
+    rmrss_subparser = subparser.add_parser('rmrss', help='Remove a RSS feed')
+    rmrss_subparser.add_argument('name', help='Comma-separated list of RSS feeds to remove')
+    rmrss_subparser.set_defaults(which='rmrss')
 
     args = parser.parse_args()
 
@@ -102,11 +143,17 @@ def main():
         current = args.current
         keyword = args.keyword
         minori.add_show(name, max_ep, keyword, current)
+    elif args.which == 'rmshow':
+        name = args.name.split(',')
+        minori.rm_show(name)
 
     if args.which == 'addrss':
         name = args.name
         url = args.url
         minori.add_rss(name, url)
+    elif args.which == 'rmrss':
+        name = args.name.split(',')
+        minori.rm_rss(name)
 
 
 if __name__ == '__main__':
